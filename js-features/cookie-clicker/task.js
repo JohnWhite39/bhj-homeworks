6c7431beca138a51de41502abd940ff0ed46d062
JohnWhite39 <124592559+JohnWhite39@users.noreply.github.com>
2024-07-17 13:10:43 +0200

    Update task.js

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -1,8 +1,21 @@
 const cookie = document.getElementById('cookie');
 const clickCounter = document.getElementById('clicker__counter');
 
+let width = 200;
+let height = 200;
+
 cookie.addEventListener('click', () => {
   clickCounter.textContent = parseInt(clickCounter.textContent) + 1;
 
-  cookie.classList.toggle('clicker__cookie--big');
+  if (width === 200 && height === 200) {
+    width = 250;
+    height = 250;
+  } else {
+    width = 200;
+    height = 200;
+  }
+
+  cookie.width = width;
+  cookie.height = height;
 });
+
