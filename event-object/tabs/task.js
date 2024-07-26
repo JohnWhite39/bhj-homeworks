f76dbfe83c790d53a55c8db3cfb8aa42e1b7be33
JohnWhite39 <124592559+JohnWhite39@users.noreply.github.com>
2024-07-26 12:42:21 +0200

    Update task.js

---
+++ event-object/tabs/task.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', function () {
+  function switchTab(tabsContainer) {
+      const tabs = tabsContainer.querySelectorAll('.tab');
+      const contents = tabsContainer.querySelectorAll('.tab__content');
+
+      tabs.forEach((tab, index) => {
+        tab.addEventListener('click', () => {
+          tabs.forEach(t => t.classList.remove('tab_active'));
+          contents.forEach(c => c.classList.remove('tab__content_active'));
+
+          tab.classList.add('tab_active');
+          contents[index].classList.add('tab__content_active');
+        });
+      });
+  }
+
+  const tabsContainers = document.querySelectorAll('.tabs');
+  tabsContainers.forEach(container => switchTab(container));
+});
